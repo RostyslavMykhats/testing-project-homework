a4fe0c5558ac1aeda372386ba0ea2b2c7b6d9cd5
RostyslavMykhats <mirosrik@gmail.com>
2023-05-13 12:37:04 +0300

    done

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 import "@/styles/globals.scss";
 import 'swiper/css';
 import 'bootstrap/dist/css/bootstrap.css'
-=======
-import "@/styles/globals.css";
-// libs styles
-import "swiper/css";
-import "bootstrap/dist/css/bootstrap.min.css";
-// layouts components
->>>>>>> 83f6140fcdc941b067ee60114a7748cc282679d9
 import Layouts from "@/layouts";
 
 export default function App({ Component, pageProps }) {
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,24 +2,14 @@
 import CardProduct from "@/components/Product/CardProduct";
 import Slide from "@/components/Slide/Slide";
 import { Inter } from "next/font/google";
-<<<<<<< HEAD
 import Image from "next/image";
 
-=======
-import { Swiper, SwiperSlide } from "swiper/react";
-import styles from "@/styles/Home.module.css";
-// custom components
-import Test from "@/components/test/test";
-// image
-import photo from "@/assets/img/pexels.jpeg";
->>>>>>> 83f6140fcdc941b067ee60114a7748cc282679d9
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home({ products }) {
   return (
     <>
-<<<<<<< HEAD
       <div className='container'>
         <div className="row">
           <div className="col-12">
@@ -47,33 +37,6 @@ export default function Home({ products }) {
         </div>
       </div>
 
-=======
-      {/* <Test text="hello world" /> */}
-      <Test>
-        <p>Test</p>
-        <ul>
-          <li>Test</li>
-          <li>Test</li>
-          <li>Test</li>
-        </ul>
-      </Test>
-      {/* image */}
-      <Image src={photo} width={200} height={200} />
-      {/* swiper */}
-      <div>
-        <Swiper
-          spaceBetween={50}
-          slidesPerView={3}
-          onSlideChange={() => console.log("slide change")}
-          onSwiper={(swiper) => console.log(swiper)}
-        >
-          <SwiperSlide>Slide 1</SwiperSlide>
-          <SwiperSlide>Slide 2</SwiperSlide>
-          <SwiperSlide>Slide 3</SwiperSlide>
-          <SwiperSlide>Slide 4</SwiperSlide>
-        </Swiper>
-      </div>
->>>>>>> 83f6140fcdc941b067ee60114a7748cc282679d9
     </>
   );
 }
